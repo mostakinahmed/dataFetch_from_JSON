18d8564bf22ee6d044ebf89dad21b3fb058365df
mostakinahmed <mostakin4556@gmail.com>
2025-01-28 15:10:33 +0600

    first

---
+++ program.js
@@ -0,0 +1,32 @@
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    productCart(data);
+    // console.log(data);
+  });
+
+function productCart(product) {
+  //console.log(product);
+  const root = document.getElementById("root");
+
+  product.forEach((element) => {
+    root.innerHTML += htmlCode(element);
+  });
+
+  function htmlCode(info) {
+    const { category, id, name, price, rating, stock } = info;
+
+    const html = `
+    <div>
+    <h1>${category}</h1>
+    <p>${name}</p>
+    <p>${id}</p>
+    <p>${price}</p>
+    <p>${rating}</p>
+    <p>${stock}</p>
+     </div>
+    `;
+
+    return html;
+  }
+}
